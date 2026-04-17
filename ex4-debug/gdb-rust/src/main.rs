40d3f024e91027c73762070d4dfddaf72e9fe8bd
kingofmongeese <nwohletz@gmail.com>
2026-04-17 13:11:34 -0600

    debug

---
+++ ex4-debug/gdb-rust/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let age = 100;
+    let distance: u8 = 255;
+
+    println!("age: {age}");
+    println!("distance: {distance}");
+}
---
+++ ex4-debug/program.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int age = 10;
+    int distance = 100;
+
+    printf("I am %d years old\n", age);
+    printf("You are %d miles away\n", distance);
+
+    return 0;
+}
